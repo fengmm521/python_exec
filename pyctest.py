7ef558fb9cb94308aca0bea6d3710c8ef90cd77a
woodcol <fengmm521@gmail.com>
2018-03-29 23:11:01 +0800

    修改从文件加载模块的代码

--- pyctest.py
+++ pyctest.py
@@ -1,1 +1,29 @@
-#!/usr/bin/env python# -*- coding: utf-8 -*-import threading  import time  xxx = 'aaa'def pyctest():    print('pyctest....')def main():    pyctest()if __name__ == '__main__':    main()
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import threading  
+import time  
+
+
+xxx = 'aaa'
+
+class TESTObj(object):
+    """docstring for TESTObj"""
+    def __init__(self, arg):
+        super(TESTObj, self).__init__()
+        self.arg = arg
+        
+    def showP(self):
+        print(self.arg)
+        print(time.time())
+
+def pyctest():
+    print('pyctest....')
+    return 'isRunOK'
+
+def main():
+    pyctest()
+
+
+if __name__ == '__main__':
+    main()
